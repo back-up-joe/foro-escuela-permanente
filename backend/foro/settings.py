c4b2f56699e62e4a2abc0496172178809db57a71
back-up-joe <jmcontrerass@gmail.com>
2026-07-12 23:32:23 -0400

    automatización de email con user y pass

--- backend/foro/settings.py
+++ backend/foro/settings.py
@@ -133,3 +133,16 @@ FILE_UPLOAD_MAX_MEMORY_SIZE = 20971520   # 20MB en bytes
 
 # También puedes agregar:
 MAX_UPLOAD_SIZE = 20971520  # 20MB
+
+# Configuración de correo electrónico
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+# Para desarrollo (no envía correos reales)
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
