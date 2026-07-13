e3f0c058fc6725ef79c2d6a7c0e16285f998d586
back-up-joe <jmcontrerass@gmail.com>
2026-07-12 23:56:12 -0400

    port email

--- backend/foro/settings.py
+++ backend/foro/settings.py
@@ -137,7 +137,7 @@ MAX_UPLOAD_SIZE = 20971520  # 20MB
 # Configuración de correo electrónico
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
